303930b9912cffcedd873dadddd73228b317276a
Yahia <yn7599@gmail.com>
2024-07-21 22:50:23 +0300

    0x00: legacy typing for the checker

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Calculate the start index and end index based on page & size"""
+import typing
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> typing.Tuple[int, int]:
     """Get the first and last indices of a page"""
     start = (page - 1) * page_size
     return start, start + page_size
