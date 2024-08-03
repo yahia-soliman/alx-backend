f35b68d03db862492a05087292810ccc2569ba52
Yahia <yn7599@gmail.com>
2024-08-03 18:34:59 +0300

    0x02: Babel get_locale

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Basic Babel setup for the Flask App"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """The configuration object
+    Usage:
+    >>> app = Flask(__name__)
+    >>> app.config.from_object(Config)
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get hte locale from the request"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def home_page():
+    """Render the Home page"""
+    return render_template("1-index.html")
